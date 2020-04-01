ca7b314db6ed4b25ddf77856e09128a583d62732
TheSharkDude <47301026+TheSharkDude@users.noreply.github.com>
2020-04-01 15:39:51 -0400

    Redirect to pages based on selected options Changed form tag to div tag

--- js/index.js
+++ js/index.js
@@ -3,10 +3,21 @@ function getOptionsAndRedirect() {
   let allRolls = document.getElementById('all-rolls').checked;
   sessionStorage.setItem('manualRoll', manualRoll.toString());
   sessionStorage.setItem('allRolls', allRolls.toString());
+
+  if (!allRolls && !manualRoll) {
+    location.reload;
+  }
+  else if (allRolls && !manualRoll) {
+    window.location.replace("../pages/roll.html");
+  }
+  else {
+    window.location.replace("../pages/foo.html");
+  }
 }
 
 function init() {
-  document.getElementById('options').reset();
+  document.getElementById('manual-roll').checked = false;
+  document.getElementById('all-rolls').checked = false;
   for (let i = 1; i <= 5; i++) {
     let die = document.getElementById('die-' + i.toString());
     die.src = "../img/dice-" + Math.floor((Math.random() * 6) + 1) + ".png";
