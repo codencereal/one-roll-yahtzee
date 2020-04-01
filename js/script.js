9d3c722598c76d9b18662b33efcedf425bf4e2b5
TheSharkDude <47301026+TheSharkDude@users.noreply.github.com>
2020-04-01 07:00:01 -0400

    Add h1 to show the outcome of the rolls

--- js/script.js
+++ js/script.js
@@ -1,2 +1,5 @@
 let manualRoll = sessionStorage.getItem('manualRoll') == 'true';
 let allRolls = sessionStorage.getItem('allRolls') == 'true';
+
+let foo = 1;
+document.getElementById('attempts').textContent = 'You got 5 ' + foo + '\'s in ' + foo + ' attempts';
