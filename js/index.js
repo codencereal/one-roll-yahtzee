5f36e4159ad70113163fff1098172a79863e1972
TheSharkDude <47301026+TheSharkDude@users.noreply.github.com>
2020-04-01 12:14:33 -0400

    Update init() to randomize the dice

--- js/index.js
+++ js/index.js
@@ -7,6 +7,10 @@ function getOptions() {
 
 function init() {
   document.getElementById('options').reset();
+  for (let i = 1; i <= 5; i++) {
+    let die = document.getElementById('die-' + i.toString());
+    die.src = "../img/dice-" + Math.floor((Math.random() * 6) + 1) + ".png";
+  }
 }
 
 init();
